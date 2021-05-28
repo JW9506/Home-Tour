4019b89d97486dc0d7378d11a1062a4f248d8fc4
JW9506 <cf.dota@gmail.com>
2021-05-28 03:34:06 -0400

    fix: some outofbound exception

--- src/command/Inspect.java
+++ src/command/Inspect.java
@@ -16,10 +16,17 @@ public class Inspect implements Command {
   @Override
   public void action(String entity) throws InvalidCommand {
     int idx = entity.charAt(0) - 'a';
-    Fixture f = g.getPlayer().getCurrentRoom().getEnvironmentFixture().get(idx);
+    List<Fixture> fixs = g.getPlayer().getCurrentRoom().getEnvironmentFixture();
+    if (fixs.size() < idx) {
+      throw Fixture.invalidCommand;
+    }
+    Fixture f = fixs.get(idx);
     List<Method> methods = f.availableMethods();
     System.out.print("\nWhat would you like to do?\n\t");
     String[] inputs = g.collectInput();
+    if (inputs[0] == null) {
+      return;
+    }
     Method mtd = f.getMethodByName(methods, inputs[0]);
     String[] params = new String[inputs.length - 1];
     for (int i = 1; i < inputs.length; ++i) {
